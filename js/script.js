de7de7fcfcd2e54f66dc9303e7954e1866b70394
udaraz <usbherath@gmail.com>
2016-09-20 16:44:06 +0530

    New SideNav added with transisions

--- js/script.js
+++ js/script.js
@@ -5,11 +5,52 @@ angular
   function dashApp($scope,$mdSidenav){
    
     $scope.toggleLeft = buildToggler('left');
-    $scope.toggleRight = buildToggler('right');
-    
+    //$scope.toggleRight = buildToggler('right');
+    $scope.is_open = true;
+      
+      
+      angular.element(document).ready(function () {
+       //alert('hello');
+           $mdSidenav('left').open();
+    });
+      
+      
+      
     function buildToggler(componentId) {
       return function() {
-        $mdSidenav(componentId).toggle();
+        $mdSidenav('left').open();
+         
+          
+          $scope.is_open = $scope.is_open === false ? true: false;
+          
+          var side = angular.element( document.querySelector( '#sideNav' ) );
+           var content = angular.element( document.querySelector( '#content' ) );
+           
+          if ($scope.is_open === false) {
+                side.addClass('textNav');
+                side.removeClass('iconNav');
+              
+                content.addClass('textContent');
+                content.removeClass('iconContent');
+            } else {
+                 side.addClass('iconNav');
+                 side.removeClass('textNav') 
+               
+                 content.addClass('iconContent');
+                content.removeClass('textContent');
+            } 
+          
+          
+          
+          
       }
     }
+      
+      
+      
+      
+      
+      
+      
+      
   }
